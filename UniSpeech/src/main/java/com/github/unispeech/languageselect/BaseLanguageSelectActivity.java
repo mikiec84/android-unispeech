1c8622c0d319dd425a70eda263758d15310c6cb9
Prashan Dharmasena <prashan14@gmail.com>
2015-01-06 16:17:18 -0500

    added API keys and resolved issues with language selection

--- UniSpeech/src/main/java/com/github/unispeech/languageselect/BaseLanguageSelectActivity.java
+++ UniSpeech/src/main/java/com/github/unispeech/languageselect/BaseLanguageSelectActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -13,10 +14,14 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.github.unispeech.R;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 
 public abstract class BaseLanguageSelectActivity extends Activity {
 
     private ListView mListView;
+
+    private GestureDetector mGestureDetector;
 
     protected abstract String getPrompt();
 
@@ -29,6 +34,7 @@ public abstract class BaseLanguageSelectActivity extends Activity {
 
         ((TextView) findViewById(R.id.lbl_prompt)).setText(getPrompt());
 
+        mGestureDetector = createGestureDetector(this);
         mListView = (ListView) findViewById(R.id.list);
         mListView.setAdapter(new LanguagesAdapter(this, SupportedSttLanguage.values()));
         mListView.setOnItemClickListener(mOnItemClickListener);
@@ -81,5 +87,50 @@ public abstract class BaseLanguageSelectActivity extends Activity {
             language = (TextView) view.findViewById(R.id.lbl_language);
             specifier = (TextView) view.findViewById(R.id.lbl_language_specifier);
         }
+    }
+
+    private GestureDetector createGestureDetector(Context context) {
+        GestureDetector gestureDetector = new GestureDetector(context);
+        //Create a base listener for generic gestures
+        gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
+            @Override
+            public boolean onGesture(Gesture gesture) {
+                if (gesture == Gesture.SWIPE_RIGHT) {
+                    // do something on right (forward) swipe
+                    mListView.setSelection(mListView.getSelectedItemPosition()+1);
+
+                    return true;
+                } else if (gesture == Gesture.SWIPE_LEFT) {
+                    // do something on left (backwards) swipe
+                    mListView.setSelection(mListView.getSelectedItemPosition()-1);
+                    return true;
+                }
+                return false;
+            }
+        });
+        gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
+            @Override
+            public void onFingerCountChanged(int previousCount, int currentCount) {
+                // do something on finger count changes
+            }
+        });
+        /*gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
+            @Override
+            public boolean onScroll(float displacement, float delta, float velocity) {
+                // do something on scrolling
+            }
+        });*/
+        return gestureDetector;
+    }
+
+    /*
+     * Send generic motion events to the gesture detector
+     */
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        if (mGestureDetector != null) {
+            return mGestureDetector.onMotionEvent(event);
+        }
+        return false;
     }
 }
